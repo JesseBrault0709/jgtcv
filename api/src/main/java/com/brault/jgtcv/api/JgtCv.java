ee75b440f1e947ed80cf8a0f56a2fb24cd8c2c66
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2021-04-25 19:36:50 -0400

    makes output dirs if needed

--- api/src/main/java/com/brault/jgtcv/api/JgtCv.java
+++ api/src/main/java/com/brault/jgtcv/api/JgtCv.java
@@ -108,13 +108,6 @@ public final class JgtCv {
     public void output(PrintedTex printedTex, File outputFile) {
         logger.traceEntry();
 
-        logger.debug("opening FileOutputStream for file {}", outputFile);
-        try (final FileOutputStream fos = new FileOutputStream(outputFile)) {
-            fos.write(printedTex.getSource().getBytes());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
         final var absoluteOutputFile = outputFile.getAbsoluteFile();
         logger.debug("absoluteOutputFile: {}", absoluteOutputFile);
         final var outputParentFile = absoluteOutputFile.getParentFile();
@@ -122,7 +115,23 @@ public final class JgtCv {
         final var outputDir = outputFile.getAbsoluteFile().getParentFile();
         logger.debug("outputDir: {}", outputDir);
 
+        final var parentFile = outputFile.getParentFile();
+        logger.debug("parentFile: {}", parentFile);
+
+        if (parentFile != null) {
+            logger.debug("making parent dirs...");
+            parentFile.mkdirs();
+        }
+
+        logger.debug("opening FileOutputStream for file {}", outputFile);
+        try (final FileOutputStream fos = new FileOutputStream(outputFile)) {
+            fos.write(printedTex.getSource().getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         printedTex.getMacroFileNamesAndSources().forEach((fileName, source) -> {
+
             final var macroOutputFile = new File(outputDir.getAbsolutePath() + File.separator + fileName + ".tex");
             logger.debug("Opening FileOutputStream for file {}", macroOutputFile);
             try (final FileOutputStream fos = new FileOutputStream(macroOutputFile)) {
