ee9ba62948e135a8ed3a4ae395f41dd8370385e2
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2021-05-29 18:30:32 -0400

    added doc

--- api/src/main/java/com/brault/jgtcv/api/model/date/DateNode.java
+++ api/src/main/java/com/brault/jgtcv/api/model/date/DateNode.java
@@ -12,49 +12,31 @@ import java.util.Optional;
 
 import static java.util.AbstractMap.SimpleEntry;
 
+/**
+ * A node representing a date. This is a terminal node,
+ * i.e., it does not contain any child nodes.
+ */
 public interface DateNode extends CVNode {
 
+    /**
+     * @return An Optional containing a Year instance, if
+     * there is enough information in the node for a Year
+     * instance, else an empty Optional.
+     */
     Optional<Year> asYear();
-    Optional<YearMonth> asYearMonth();
-    Optional<LocalDate> asLocalDate();
 
-//
-//    Map<String, Integer> MONTH_INDICES = Map.ofEntries(
-//            new SimpleEntry<>("January", 1),
-//            new SimpleEntry<>("February", 2),
-//            new SimpleEntry<>("March", 3),
-//            new SimpleEntry<>("April", 4),
-//            new SimpleEntry<>("May", 5),
-//            new SimpleEntry<>("June", 6),
-//            new SimpleEntry<>("July", 7),
-//            new SimpleEntry<>("August", 8),
-//            new SimpleEntry<>("September", 9),
-//            new SimpleEntry<>("October", 10),
-//            new SimpleEntry<>("November", 11),
-//            new SimpleEntry<>("December", 12)
-//    );
-//
-//    Comparator<DateNode> COMPARATOR =
-//            Comparator.comparingInt((DateNode dn) -> dn.getYear().orElse(0))
-//                    .thenComparing((DateNode dn1, DateNode dn2) -> {
-//                        if (dn1.getMonth().isEmpty()) {
-//                            return -1;
-//                        } else if (dn2.getMonth().isEmpty()) {
-//                            return 1;
-//                        }
-//                        final int dn1MonthIndex = MONTH_INDICES.get(dn1.getMonth().get());
-//                        final int dn2MonthIndex = MONTH_INDICES.get(dn2.getMonth().get());
-//                        return Integer.compare(dn1MonthIndex, dn2MonthIndex);
-//                    })
-//                    .thenComparingInt(dn -> dn.getDay().orElse(0));
-//
-//    Optional<Integer> getDay();
-//    Optional<String> getMonth();
-//    Optional<Integer> getYear();
-//
-//    @Override
-//    default int compareTo(DateNode o) {
-//        return COMPARATOR.compare(this, o);
-//    }
+    /**
+     * @return An Optional containing a YearMonth instance,
+     * if there is enough information in the node for such
+     * an instance, else an empty Optional.
+     */
+    Optional<YearMonth> asYearMonth();
+
+    /**
+     * @return An Optional containing a LocalDate instance,
+     * if there is enough information in the node for such
+     * an instance, else an empty Optional.
+     */
+    Optional<LocalDate> asLocalDate();
 
 }
