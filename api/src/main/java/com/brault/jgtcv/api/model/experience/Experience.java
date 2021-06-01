ff2d0f8275b4d43237d3c729dcc28b1a471f775b
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2021-06-01 19:55:55 -0400

    Added Builder interface and ForwardingBuilders

--- api/src/main/java/com/brault/jgtcv/api/model/experience/Experience.java
+++ api/src/main/java/com/brault/jgtcv/api/model/experience/Experience.java
@@ -1,12 +1,106 @@
 package com.brault.jgtcv.api.model.experience;
 
+import com.brault.jgtcv.api.builder.CVNodeBuilder;
 import com.brault.jgtcv.api.model.date.DateNode;
 import com.brault.jgtcv.api.model.duties.Duties;
 import com.brault.jgtcv.api.model.node.CVNode;
+import groovy.lang.Closure;
+import groovy.lang.DelegatesTo;
 
+import java.util.Map;
 import java.util.Optional;
 
 public interface Experience extends CVNode {
+
+    interface Builder extends CVNodeBuilder<Experience> {
+        Builder institution(String institution);
+        String institution();
+
+        Builder date(DateNode date);
+        Builder date(Map<String, Object> dateMap);
+        DateNode date();
+
+        Builder role(String role);
+        String role();
+
+        Builder description(String description);
+        String description();
+
+        Builder duties(Duties duties);
+        Builder duties(
+                @DelegatesTo(value = Duties.Builder.class, strategy = Closure.DELEGATE_ONLY)
+                Closure<?> cl
+        );
+        Duties duties();
+    }
+
+    abstract class ForwardingBuilder implements Builder {
+
+        private final Builder b;
+
+        public ForwardingBuilder(Builder b) { this.b = b; }
+
+        @Override
+        public Builder institution(String institution) {
+            return b.institution(institution);
+        }
+
+        @Override
+        public String institution() {
+            return b.institution();
+        }
+
+        @Override
+        public Builder date(DateNode date) {
+            return b.date(date);
+        }
+
+        @Override
+        public Builder date(Map<String, Object> dateMap) {
+            return b.date(dateMap);
+        }
+
+        @Override
+        public DateNode date() {
+            return b.date();
+        }
+
+        @Override
+        public Builder role(String role) {
+            return b.role(role);
+        }
+
+        @Override
+        public String role() {
+            return b.role();
+        }
+
+        @Override
+        public Builder description(String description) {
+            return b.description(description);
+        }
+
+        @Override
+        public String description() {
+            return b.description();
+        }
+
+        @Override
+        public Builder duties(Duties duties) {
+            return b.duties(duties);
+        }
+
+        @Override
+        public Builder duties(Closure<?> cl) {
+            return b.duties(cl);
+        }
+
+        @Override
+        public Duties duties() {
+            return b.duties();
+        }
+
+    }
 
     String getInstitution();
     DateNode getDate();
--- api/src/main/java/com/brault/jgtcv/api/model/workshop/Workshop.java
+++ api/src/main/java/com/brault/jgtcv/api/model/workshop/Workshop.java
@@ -1,12 +1,88 @@
 package com.brault.jgtcv.api.model.workshop;
 
+import com.brault.jgtcv.api.builder.CVNodeBuilder;
 import com.brault.jgtcv.api.model.date.DateNode;
 import com.brault.jgtcv.api.model.node.CVNode;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Optional;
 
 public interface Workshop extends CVNode {
+
+    interface Builder extends CVNodeBuilder<Workshop> {
+        Builder workshopName(String workshopName);
+        String workshopName();
+
+        Builder date(DateNode date);
+        Builder date(Map<String, Object> map);
+        DateNode date();
+
+        Builder location(String location);
+        String location();
+
+        Builder instructor(String instructor);
+        Builder instructors(Collection<String> instructors);
+        Collection<String> instructors();
+    }
+
+    abstract class ForwardingBuilder implements Builder {
+
+        private final Builder b;
+
+        public ForwardingBuilder(Builder b) { this.b = b; }
+
+        @Override
+        public Builder workshopName(String workshopName) {
+            return b.workshopName(workshopName);
+        }
+
+        @Override
+        public String workshopName() {
+            return b.workshopName();
+        }
+
+        @Override
+        public Builder date(DateNode date) {
+            return b.date(date);
+        }
+
+        @Override
+        public Builder date(Map<String, Object> map) {
+            return b.date(map);
+        }
+
+        @Override
+        public DateNode date() {
+            return b.date();
+        }
+
+        @Override
+        public Builder location(String location) {
+            return b.location(location);
+        }
+
+        @Override
+        public String location() {
+            return b.location();
+        }
+
+        @Override
+        public Builder instructor(String instructor) {
+            return b.instructor(instructor);
+        }
+
+        @Override
+        public Builder instructors(Collection<String> instructors) {
+            return b.instructors(instructors);
+        }
+
+        @Override
+        public Collection<String> instructors() {
+            return b.instructors();
+        }
+
+    }
 
     String getWorkshopName();
     DateNode getDate();
